c26c3fedec31e0365299e9b6b3a6eb4fda6b0f5a
Anthropoworphous <kevin1050914@gmail.com>
2021-12-23 06:28:49 -0500

    added ability to change logger

--- src/main/java/com/github/anthropoworphous/commandlib/CMDLib.java
+++ src/main/java/com/github/anthropoworphous/commandlib/CMDLib.java
@@ -4,15 +4,18 @@ import com.github.anthropoworphous.commandlib.adaptor.processor.CMDExecutor;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class CMDLib extends JavaPlugin {
 
     private static CMDExecutor executor;
     public static CMDExecutor getExecutor() { return executor; }
 
     public static void log(String str) {
-        Bukkit.getLogger().info("[CMDLib]: " + str);
+        logger.info("[CMDLib]: " + str);
     }
 
+    private static Logger logger = Bukkit.getLogger();
     private static boolean logDetails = false;
     public static boolean logDetails() { return logDetails; }
 
@@ -23,5 +26,12 @@ public final class CMDLib extends JavaPlugin {
         getServer().getConsoleSender().sendMessage("CommandLib loaded!");
     }
 
-    public static void setLogDetails(boolean logDetails) { CMDLib.logDetails = logDetails; }
+    public static void setLogDetails(boolean logDetails, Logger logger) {
+        CMDLib.logDetails = logDetails;
+        CMDLib.logger = logger;
+    }
+
+    public static void setLogDetails(boolean logDetails) {
+        CMDLib.logDetails = logDetails;
+    }
 }
