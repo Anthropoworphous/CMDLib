f13b43e92aff2774d56952aae60ea5103fa0430c
Anthropoworphous <kevin1050914@gmail.com>
2022-03-25 12:45:18 -0400

    e

--- src/main/java/com/github/anthropoworphous/cmdlib/CMDLib.java
+++ src/main/java/com/github/anthropoworphous/cmdlib/CMDLib.java
@@ -6,7 +6,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class CMDLib extends JavaPlugin {
     @Override
     public void onEnable() {
-        getServer().getConsoleSender().sendMessage("CommandLib loaded!");
-        getServer().getConsoleSender().sendMessage("Version: " + Bukkit.getBukkitVersion());
+        getServer().getConsoleSender().sendMessage("CommandLib loaded!\nVersion: " + Bukkit.getBukkitVersion());
     }
 }
