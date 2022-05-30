6edeba3acbe0472cf6ed7578de7356c61778a665
Anthropoworphous <kevin1050914@gmail.com>
2022-05-30 13:35:23 -0400

    time for a rewrite
    Took 6 hours 26 minutes

--- src/main/java/com/github/anthropoworphous/cmdlib/arg/type/lonetypes/implementation/types/CustomLoneVar.java
+++
@@ -1,9 +0,0 @@
-package com.github.anthropoworphous.cmdlib.arg.type.lonetypes.implementation.types;
-
-import com.github.anthropoworphous.cmdlib.arg.type.lonetypes.implementation.LoneTypes;
-
-public abstract class CustomLoneVar<T> extends LoneTypes<T> {
-    public CustomLoneVar(String readableName, String readableDescription) {
-        super(readableName, readableDescription);
-    }
-}
