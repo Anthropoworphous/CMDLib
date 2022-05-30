497677c189ce945f2c016caf63b7a4e8456b75cf
Anthropoworphous <kevin1050914@gmail.com>
2022-05-30 19:15:03 -0400

    auto complete added
    Took 39 seconds

--- src/main/java/com/github/anthropoworphous/cmdlib/command/variable/addition/impl/AutoComplete.java
+++ src/main/java/com/github/anthropoworphous/cmdlib/command/variable/addition/impl/AutoComplete.java
@@ -5,12 +5,12 @@ import com.github.anthropoworphous.cmdlib.command.variable.addition.Addition;
 import com.github.anthropoworphous.cmdlib.command.variable.addition.VarFacade;
 
 import java.util.List;
-import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class AutoComplete<T> extends Addition<T> {
-    private final Function<String, List<String>> autoComplete;
+    private final Supplier<List<String>> autoComplete;
 
-    public AutoComplete(Function<String, List<String>> autoComplete) {
+    public AutoComplete(Supplier<List<String>> autoComplete) {
         this.autoComplete = autoComplete;
     }
 
@@ -18,8 +18,8 @@ public class AutoComplete<T> extends Addition<T> {
     public Var<T> add(Var<T> var) {
         return new VarFacade<>(var) {
             @Override
-            public List<String> autoComplete(String input) {
-                return autoComplete.apply(input);
+            public List<String> autoComplete() {
+                return autoComplete.get();
             }
         };
     }
