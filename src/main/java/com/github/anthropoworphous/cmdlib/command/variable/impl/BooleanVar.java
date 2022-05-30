28ffd2f93774f6627115ac8bc293d51c033a5ee3
Anthropoworphous <kevin1050914@gmail.com>
2022-05-30 14:01:29 -0400

    base type
    Took 5 seconds

---
+++ src/main/java/com/github/anthropoworphous/cmdlib/command/variable/impl/BooleanVar.java
@@ -0,0 +1,16 @@
+package com.github.anthropoworphous.cmdlib.command.variable.impl;
+
+import com.github.anthropoworphous.cmdlib.command.variable.Var;
+
+import java.util.List;
+
+public class BooleanVar extends Var<Boolean> {
+    @Override
+    public Boolean convert(List<String> input) throws IllegalArgumentException {
+        return switch (input.get(0).toLowerCase()) {
+            case "true" -> true;
+            case "false" -> false;
+            default -> throw new IllegalArgumentException();
+        };
+    }
+}
