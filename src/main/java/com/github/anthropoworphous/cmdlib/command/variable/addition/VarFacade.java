7a7cf5c62155e45f71c474c4874e908ce5d103e0
Anthropoworphous <kevin1050914@gmail.com>
2022-05-30 13:37:16 -0400

    for simpler way to override method in var in additional stuff
    Took 2 minutes

---
+++ src/main/java/com/github/anthropoworphous/cmdlib/command/variable/addition/VarFacade.java
@@ -0,0 +1,27 @@
+package com.github.anthropoworphous.cmdlib.command.variable.addition;
+
+import com.github.anthropoworphous.cmdlib.command.variable.Var;
+
+import java.util.List;
+
+public class VarFacade<T> extends Var<T> {
+    private final Var<T> var;
+
+    public VarFacade(Var<T> var) {
+        this.var = var;
+    }
+
+    // Facade
+    @Override
+    public T convert(List<String> input) throws IllegalArgumentException {
+        return var.convert(input);
+    }
+    @Override
+    public int size() {
+        return var.size();
+    }
+    @Override
+    public List<String> autoComplete(String input) {
+        return var.autoComplete(input);
+    }
+}
